6889f86c582dcd8c087f480c82401293c71d4a10
Jeviier <jvrrfl7@gmail.com>
2024-03-04 14:20:51 +0700

    two

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,6 +24,70 @@
     <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
-    
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center ">
+            <h1 class="logo me-auto"><a href="index.html">BSNP</a></h1>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="getstarted scrollto" href="/login">LOG IN</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+        </div>
+    </header>
+
+    <section id="hero" class="d-flex align-items-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"
+                 data-aos="fade-up" data-aos-delay="200">
+                 <h1>Welcome To Classroom BNSP</h1>
+                 <h2>Jakarta Poesat</h2>
+                 <div class="d-flex justify-content-center justify-content-lg-start ">
+                    <a href="#about" class="btn-get-started scrollto"> Ayo Mulai!!!!!</a>
+                 </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+                    <img src="assets/img/logaotamsis.png" class="img-fluid animated" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <main id="main">
+        <footer id="footer">
+
+            <div class="footer-top">
+                <div class="container">
+                    <div class="row">
+                    </div>
+                </div>
+            </div>
+
+            <div class="container footer-bottom clearfix">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>BNSP-TUGAS</span></strong>All Right Reserved
+                </div>
+                <div class="credits">
+                    Designed by <a href="https://bootsrapmade.com">BootsrapMade</a>
+                </div>
+            </div>
+        </footer>
+
+        <div id="preloader"></div>
+        <a href="#" class="back-to-top d-flex align center-justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    </main>
+
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <script src="assets/js/main.js"></script>
 </body>
 </html>
